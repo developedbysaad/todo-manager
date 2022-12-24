7a2e9cac8b0f8ac4176820758a59ce72f9d947d5
Mohd Saad <developedbysaad@gmail.com>
2022-12-24 23:01:30 +0530

    Add sequelize validation

--- models/todo.js
+++ models/todo.js
@@ -89,7 +89,13 @@ module.exports = (sequelize, DataTypes) => {
   }
   Todo.init(
     {
-      title: DataTypes.STRING,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
       dueDate: DataTypes.DATEONLY,
       completed: DataTypes.BOOLEAN,
     },
